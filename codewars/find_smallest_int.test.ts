ac915e0cfbedcc26e6e30e5e8416b33a61a350af
poy <ghhh7731@gmail.com>
2023-12-20 13:26:35 +0700

    ci: :page_facing_up: edit test

--- codewars/find_smallest_int.test.ts
+++ codewars/find_smallest_int.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from "bun:test";
 import { findSmallestInt } from "./find_smallest_int";
 
-test("Should return the smallest int -6", () => {
+test("Should return the smallest int 2", () => {
 	expect(findSmallestInt([34, 15, 88, 2])).toEqual(2);
 });
 
-test("Should return the smallest int 5 ", () => {
+test("Should return the smallest int -345 ", () => {
 	expect(findSmallestInt([34, -345, -1, 100])).toEqual(-345);
 });
