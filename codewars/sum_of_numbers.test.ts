5966bdfe446f502f32ddfa19780251767598d946
poy <ghhh7731@gmail.com>
2023-12-20 14:33:32 +0700

    ci: :memo: add test number new

--- codewars/sum_of_numbers.test.ts
+++ codewars/sum_of_numbers.test.ts
@@ -1,1 +1,21 @@
-// test sum_of_numbers.test
+import { expect, test } from "bun:test";
+import { getSum } from "./sum_of_numbers";
+
+test("Should return the int 1 ", () => {
+	expect(getSum(1, 0)).toBe(1);
+});
+test("Should return the int 3 ", () => {
+	expect(getSum(1, 2)).toBe(3);
+});
+
+test("Should return the int 1 ", () => {
+	expect(getSum(0, 1)).toBe(1);
+});
+
+test("Should return the int 1 ", () => {
+	expect(getSum(1, 1)).toBe(1);
+});
+
+test("Should return the int -1 ", () => {
+	expect(getSum(-1, 0)).toBe(-1);
+});
