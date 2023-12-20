dbae6ca98a65a19fca23761a66df5fe7234661e4
trinisar <trintn.i@gmail.com>
2023-12-20 21:49:27 +0700

    add more test cases

--- codewars/03_ reverse_array.test.ts
+++ codewars/03_ reverse_array.test.ts
@@ -4,3 +4,15 @@ import { digitize } from "./03_reverse_array";
 test("should get [3,2,1] from 123", () => {
     expect(digitize(123)).toEqual([3, 2, 1]);
 });
+
+test("should get [1,2,3,4,5] from 54321", () => {
+    expect(digitize(54321)).toEqual([1,2,3,4,5]);
+});
+
+test("should get [1,3,5,7,9] from 97531", () => {
+    expect(digitize(97531)).toEqual([1,3,5,7,9]);
+});
+
+test("should get [0] from 0", () => {
+    expect(digitize(0)).toEqual([0]);
+});
