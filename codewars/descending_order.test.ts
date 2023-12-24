9e3d771a17a753d550a2799d754ce8e6e7393d89
poy <ghhh7731@gmail.com>
2023-12-24 13:42:00 +0700

    ci: :memo: add test

--- codewars/descending_order.test.ts
+++ codewars/descending_order.test.ts
@@ -16,3 +16,11 @@ test("Returns numbers in descending order 8655421 ", () => {
 test("Returns numbers in descending order 9983411 ", () => {
 	expect(descendingOrder(3499811)).toEqual(9984311);
 });
+
+test("Returns numbers in descending order 54421 ", () => {
+	expect(descendingOrder(42145)).toEqual(54421);
+});
+
+test("Returns numbers in descending order  6543211 ", () => {
+	expect(descendingOrder(145263)).toEqual(654321);
+});
