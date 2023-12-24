1ce88d1cd60e807d9a92fe9c02129f025a577d7a
poy <ghhh7731@gmail.com>
2023-12-24 17:43:59 +0700

    ci: :memo: edit test oddOrEven

--- codewars/odd_or_even.test.ts
+++ codewars/odd_or_even.test.ts
@@ -1,1 +1,14 @@
-// test4
+import { expect, test } from "bun:test";
+import { oddOrEven } from "./odd_or_even";
+
+test("Should return the คู่", () => {
+	expect(oddOrEven([0])).toEqual("คู่");
+});
+
+test("Should return the คี่", () => {
+	expect(oddOrEven([0, 1, 4])).toEqual("คี่");
+});
+
+test("Should return the คู่", () => {
+	expect(oddOrEven([0, -1, -5])).toEqual("คู่");
+});
