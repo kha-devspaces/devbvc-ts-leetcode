f1d2d2f5095cf58e240d7514dbf045d74d903789
Tiwthawat Somaumchan <liew8444@gmail.com>
2024-01-04 14:46:12 +0700

    feat: :lipstick: sumPairs edit

--- codewars/sum_of_pairs.ts
+++ codewars/sum_of_pairs.ts
@@ -1,3 +1,20 @@
 export function sumPairs(ints: number[], s: number): [number, number] | void {
-  return undefined; // your code here...
+  const seenNumbers: { [key: number]: number } = {};
+  const result = ints.reduce<[number, number] | void>(
+    (acc, currentNumber, currentIndex) => {
+      const complement = s - currentNumber;
+      if (complement in seenNumbers) {
+        return [complement, currentIndex];
+      }
+
+      seenNumbers[currentNumber] = currentIndex;
+
+      return acc;
+    },
+    undefined
+  );
+
+  return result;
 }
+
+console.log("got ->", sumPairs([4, 3, 2, 3, 4], 5));
