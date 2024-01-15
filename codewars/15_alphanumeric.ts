7a2ea5742612e475be5e3ba8c09f2df0e468aa10
trinisar <trintn.i@gmail.com>
2024-01-16 00:18:18 +0700

    solved alphanumeric

---
+++ codewars/15_alphanumeric.ts
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/typescript
+
+// In this example you have to validate if a user input string is alphanumeric. 
+// The string has the following conditions to be alphanumeric:
+// At least one character ("" is not valid)
+// Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+// No whitespaces / underscore
+
+// 1. split string into array
+// 2. test if every character match alphanumeric condition
+// 3. return true/false
+
+// Expectation
+// "Mazinkaiser" -> true
+// "hello world_" -> false
+// "PassW0rd" -> true
+// "     " -> false
+
+export function alphanumeric(string: string): boolean {
+    return string.split('').every(char => /[a-z0-9]/i.test(char));
+  }
+  
